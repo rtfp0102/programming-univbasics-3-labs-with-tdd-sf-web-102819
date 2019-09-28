570aa76e74690b25769e1e3313c641f73cbe5627
rtfp0102 <rtfp0102@gmail.com>
2019-09-28 21:20:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
  first_number = 78
 second_number = 8 
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
